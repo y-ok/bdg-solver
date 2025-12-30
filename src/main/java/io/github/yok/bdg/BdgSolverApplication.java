aba4d1cc08ec22297bc7c942fd0d8242f90730da
y-ok <fairsky1985@gmail.com>
2025-12-31 03:51:16 +0900

    Initial commit

---
+++ src/main/java/io/github/yok/bdg/BdgSolverApplication.java
@@ -0,0 +1,13 @@
+package io.github.yok.bdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdgSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdgSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yok/bdg/BdgSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yok.bdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdgSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
